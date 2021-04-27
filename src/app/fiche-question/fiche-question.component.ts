df8bda6b7f430dc93c99fbaf5a5ea3c8fd521274
Wassim Gam <gam_wassim@hotmail.com>
2021-04-27 13:22:34 +0100

    bugFix1

--- src/app/fiche-question/fiche-question.component.ts
+++ src/app/fiche-question/fiche-question.component.ts
@@ -40,173 +40,7 @@ export class FicheQuestionComponent implements OnInit {
   pagedItems = [];
   currentPage = 0;
   listPropositions = [];
-  questions = [
-    {
-      "is_template": false,
-      "order": 1,
-      "type": "TEXT",
-      "status": "TEXT",
-      "_id": "607eb8111e06e53dfc6f3963",
-      "name": "do you agree with the goverment's latest decisions ?",
-      "created_at": "2021-04-20T11:16:33.143Z",
-      "__v": 0,
-      "propositions": []
-    },
-    {
-      "is_template": false,
-      "order": 1,
-      "type": "CHECKBOX",
-      "status": "DATE",
-      "_id": "607eb9721e06e53dfc6f3965",
-      "name": "----do you agree with the goverment's latest decisions ?",
-      "created_at": "2021-04-20T11:22:26.555Z",
-      "__v": 0,
-      "propositions": []
-    },
-    {
-      "is_template": false,
-      "order": 1,
-      "type": "TEXT",
-      "status": "CREATED",
-      "_id": "607eb9bd1e06e53dfc6f3966",
-      "name": "do you agree with the goverment's latest decisions ?",
-      "created_at": "2021-04-20T11:23:41.224Z",
-      "__v": 0,
-      "propositions": []
-    },
-    {
-      "is_template": false,
-      "order": 1,
-      "type": "LIST",
-      "status": "CHECKBOX",
-      "_id": "607ed63a5e8c000dce0fbb57",
-      "propositions": [
-        {
-          "texte": "ok",
-          "score": 5,
-          "note": 2.5
-        },
-        {
-          "texte": "ok",
-          "score": 8,
-          "note": 2.6
-        },
-        {
-          "texte": "nok",
-          "score": 5,
-          "note": 2.6
-        }
-      ],
-      "name": "do you agree with the goverment's latest decisions ..//§§??",
-      "created_at": "2021-04-20T13:25:14.321Z",
-      "__v": 0
-    },
-    {
-      "is_template": false,
-      "order": 1,
-      "type": "CHECKBOX",
-      "status": "CHECKBOX",
-      "_id": "607ed6475e8c000dce0fbb58",
-      "propositions": [
-        {
-          "texte": "oui",
-          "score": 0,
-          "note": 0
-        }
-      ],
-      "name": "do you agree with the goverment's latest decisions ?",
-      "created_at": "2021-04-20T13:25:27.349Z",
-      "__v": 0
-    },
-    {
-      "is_template": false,
-      "order": 1,
-      "type": "CHECKBOX",
-      "status": "TEXT",
-      "_id": "607ed69d5e8c000dce0fbb59",
-      "propositions": [
-        {
-          "texte": "ok",
-          "score": 5,
-          "note": 2.5
-        },
-        {
-          "texte": "ok",
-          "score": 5,
-          "note": 2.5
-        }
-      ],
-      "name": "الجمهور يحب يسألك   لاباس؟؟ ?//",
-      "created_at": "2021-04-20T13:26:53.619Z",
-      "__v": 0
-    },
-    {
-      "is_template": false,
-      "order": 1,
-      "type": "CHECKBOX",
-      "status": "CREATED",
-      "_id": "607ed6bb5e8c000dce0fbb5a",
-      "propositions": [
-        {
-          "texte": "لاباس",
-          "score": 5,
-          "note": 2.5
-        },
-        {
-          "texte": "لاباسلاباس",
-          "score": 5,
-          "note": 2.5
-        }
-      ],
-      "name": "الجمهور يحب يسألك   لاباس؟؟ ?",
-      "created_at": "2021-04-20T13:27:23.176Z",
-      "__v": 0
-    },
-    {
-      "is_template": false,
-      "order": 1,
-      "type": "LIST",
-      "status": "CREATED",
-      "_id": "60845d45ad1b56241f54d404",
-      "name": "test 123",
-      "propositions": [
-        {
-          "texte": "A",
-          "score": 0,
-          "note": 0
-        },
-        {
-          "texte": "AA",
-          "score": 0,
-          "note": 0
-        }
-      ],
-      "created_at": "2021-04-24T18:02:45.108Z",
-      "__v": 0
-    },
-    {
-      "is_template": false,
-      "order": 1,
-      "type": "CHECKBOX",
-      "status": "CREATED",
-      "_id": "60857b03ad1b56241f54d409",
-      "name": "Question 12",
-      "propositions": [
-        {
-          "texte": "A",
-          "score": 0,
-          "note": 0
-        },
-        {
-          "texte": "B",
-          "score": 0,
-          "note": 0
-        }
-      ],
-      "created_at": "2021-04-25T14:21:55.855Z",
-      "__v": 0
-    }];
-
+  questions = [];
 
   // calculate page in place
   groupToPages = function () {
@@ -226,17 +60,16 @@ export class FicheQuestionComponent implements OnInit {
 
   range = function (size, start, end) {
     var ret = [];
-    console.log(size, start, end);
+   // console.log(size, start, end);
 
     if (size < end) {
       end = size;
-      // this.steps=size;
       start = size - this.steps;
     }
     for (var i = start; i < end; i++) {
       ret.push(i);
     }
-    console.log(ret);
+    //console.log(ret);
     return ret;
   };
 
@@ -272,7 +105,7 @@ export class FicheQuestionComponent implements OnInit {
     this.ficheQuestionService.getAllQuestions().subscribe(questions => {
 
       this.questions = questions;
-      console.log("portfolios", questions);
+      console.log("questions", questions);
       console.log("this.questions", this.questions);
       this.groupToPages();
 
@@ -280,21 +113,20 @@ export class FicheQuestionComponent implements OnInit {
   }
 
   ajouterQuestion() {
-    /* let reponses=[];
+    
+     let question={'name': this.questionForm.get('question').value
+     ,'type':this.questionForm.get('type').value,
+     'propositions':this.listPropositions}
 
-     for(let i=0;i<this.listPropositions.length;i++)
-     reponses.push(this.listPropositions[i].texte);**/
-
-    let question = '{"name":"' + this.questionForm.get('question').value + '","type":"' + this.questionForm.get('type').value + '","propositions": ' + JSON.stringify(this.listPropositions) + '}';
     this.ficheQuestionService.addQuestion(question).subscribe(questions => {
 
-      this.questions = questions;
-      console.log("portfolios", questions);
-      console.log("this.questions", this.questions);
-      this.clearForm();
+      
       this.getAllQuestions();
+      this.addModal.hide();
+      this.clearForm();
 
     });
+
   }
 
   updateProposition(event: any, i) {
@@ -324,16 +156,19 @@ export class FicheQuestionComponent implements OnInit {
 
   updateQuestion() {
 
-    let reponse = [];
-    for (let i = 0; i < this.listPropositions.length; i++)
-      reponse.push(this.listPropositions[i].texte);
+    
+    if(this.questionForm.get('type').value!="CHECKBOX"&&this.questionForm.get('type').value!="LIST")
+      this.listPropositions=[];
 
-    let question = '{"name":"' + this.questionForm.get('question').value + '","type":"' + this.questionForm.get('type').value + '","reponses": ' + JSON.stringify(reponse) + '}';
-    this.ficheQuestionService.updateQuestion(this.questionUpdateId, question).subscribe(result => {
+      let question={name: this.questionForm.get('question').value
+      ,type:this.questionForm.get('type').value,
+      propositions:this.listPropositions}
+
+    this.ficheQuestionService.updateQuestion(this.questionUpdateId,question).subscribe(result => {
       console.log("result: ", result)
+      this.getAllQuestions();
       this.modifyModal.hide();
       this.clearForm();
-      this.getAllQuestions();
 
     });
 
@@ -360,6 +195,7 @@ export class FicheQuestionComponent implements OnInit {
   clearForm() {
     this.questionForm.get('question').setValue('');
     this.questionForm.get('type').setValue('');
+    this.listPropositions = [];
   }
 
   unsavedChanges(ModalName) {
